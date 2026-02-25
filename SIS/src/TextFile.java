c303f341e9634a52f43d84bcf87d7eee954cba18
grace.maffoni <grace.maffoni@MU-C1-17.mullen.pvt.k12.co.us>
2026-02-25 15:01:48 -0700

    testing

--- SIS/src/TextFile.java
+++ SIS/src/TextFile.java
@@ -5,17 +5,26 @@ import java.util.Scanner;
 
 public class TextFile 
 	{
+		static ArrayList<Student> directory = new ArrayList<Student>();
+		
 		public static void textFile() throws IOException
 		{
 			Scanner studentList = new Scanner(new File("StudentList.txt"));
 			
-			ArrayList<Student> directory = new ArrayList<Student>();
+
 			
 			while (studentList.hasNext())
 				{
 					String line = studentList.nextLine();
-					System.out.println(line);
-					
+					String[] data = line.split(" ");
+					System.out.println(data.length);
+
+					directory.add(new Student(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], 0, 1, 2, 3));
 				}
+
+		}
+		public static void tester()
+		{
+			System.out.println(directory.get(0).getFirstName());
 		}
 }
